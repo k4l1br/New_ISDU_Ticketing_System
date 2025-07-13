53b674fbb0b5fb6c97cd8a739c6313e3d12add41
Mel Lyka <melvallega4@gmail.com>
2025-07-13 18:56:17 +0800

    user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ticketController;
 use App\Http\Controllers\reqOfficeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PositionController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -24,14 +25,33 @@ if (method_exists(Auth::class, 'routes')) {
 }
 
 // Protected routes
-// Authenticated Routes
 Route::middleware(['auth'])->group(function () {
-   Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Tickets
     Route::get('/tickets', [ticketController::class, 'index'])->name('tickets');
     Route::get('/tickets/create', [ticketController::class, 'create'])->name('pages.ticket.create');
     Route::resource('ticket', ticketController::class);
 
-    // Requesting Office routes
+    // Requesting Office
     Route::get('/reqOffice/create', [reqOfficeController::class, 'create'])->name('reqOffice.create');
     Route::resource('/reqOffice', reqOfficeController::class);
+
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard-data', [DashboardController::class, 'getData']);
+    Route::get('/dashboard-per-unit', [DashboardController::class, 'getTicketsPerUnit']);
+    Route::get('/dashboard-tasks-report', [DashboardController::class, 'tasksReport']);
+
+    // Position
+    Route::get('/position', [PositionController::class, 'index'])->name('position.index');
+    Route::resource('position', PositionController::class);
+
+     // User Profile
+    Route::get('/profile', [UserController::class, 'showProfile'])->name('profile');
+
+    // Admin User Management
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    });
 });
