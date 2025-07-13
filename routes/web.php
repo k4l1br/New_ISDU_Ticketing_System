e0bd07db216457a606be4a2e42514242c6d9b2e1
Mel Lyka <melvallega4@gmail.com>
2025-07-13 17:55:43 +0800

    user

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ticketController;
 use App\Http\Controllers\reqOfficeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PositionController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 use App\Models\ticket;
 
 // Redirect root to login
@@ -24,8 +24,10 @@ if (method_exists(Auth::class, 'routes')) {
     Auth::routes();
 }
 
-// Protected routes
+// Protected routes (require login)
 Route::middleware(['auth'])->group(function () {
+
+    // Home
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
     // Tickets
@@ -55,3 +57,4 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/position', [PositionController::class, 'index'])->name('position.index');
     Route::resource('position', PositionController::class);
 });
+
